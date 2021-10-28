749dbf1592ad035fbe8f87a38e9d34c953c688ab
JingW <wangjingwj@gmail.com>
2021-10-28 10:30:39 +0800

    malloc lab failing 9 and 10

--- labs/malloclab-handout/mm.c
+++ labs/malloclab-handout/mm.c
@@ -71,7 +71,11 @@ team_t team = {
 #define NEXT_BLKP(bp)   ((char *)(bp) + GET_SIZE(HDRP(bp)))
 #define PREV_BLKP(bp)   ((char *)(bp) - GET_SIZE(HDRP(bp)-WSIZE))
 
+#define VERBOSE 0
+
 static void *heap_listp;
+static void *heap_lastp;
+static int nblocks = 0;
 // Merge free blocks
 static void *coalesce(void *bp)
 {
@@ -107,12 +111,11 @@ static void *coalesce(void *bp)
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
-
     return bp;
     
 }
 // Size is aligned to DSIZE (8 bytes)
-static void *find_fit(size_t asize)
+static void *find_first_fit(size_t asize)
 {
     void *sp = heap_listp;
     void *bp = NULL;
@@ -132,6 +135,60 @@ static void *find_fit(size_t asize)
         break;        
     }
     return bp;
+}
+// Size is aligned to DSIZE (8 bytes)
+static void *find_next_fit(size_t asize)
+{
+    // find from last alloc block to epilogure
+    void *sp = heap_lastp;
+    void *bp = NULL;
+    while (GET_SIZE(HDRP(sp)) != 0)
+    {
+
+        if (GET_ALLOC(HDRP(sp)))
+        {
+            sp = NEXT_BLKP(sp);
+            continue;
+        }
+        if (GET_SIZE(HDRP(sp)) < asize)
+        {
+            sp = NEXT_BLKP(sp);
+            continue;
+        }
+        bp = sp;
+        heap_lastp = sp;        
+        if (VERBOSE)
+        {
+            printf("Last alloc pointer %p\n", HDRP(heap_lastp));
+        }
+        
+        return bp;
+    }
+
+    sp = heap_listp;
+    // find from progoure to last alloc
+    while ( sp != heap_lastp )
+    {
+        if (GET_ALLOC(HDRP(sp)))
+        {
+            sp = NEXT_BLKP(sp);
+            continue;
+        }
+        if (GET_SIZE(HDRP(sp)) < asize)
+        {
+            sp = NEXT_BLKP(sp);
+            continue;
+        }
+        bp = sp;
+        heap_lastp = sp;
+        if (VERBOSE)
+        {
+            printf("Last alloc pointer %p\n", HDRP(heap_lastp));
+        }
+        break;
+    }
+    return bp;
+    
 }
 
 static void place(void *bp, size_t asize)
@@ -161,6 +218,7 @@ static void *extend_heap(size_t words)
     PUT(FTRP(bp), PACK(size, 0));
     PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
+    nblocks += 1;
     return coalesce(bp);
 }
 
@@ -178,14 +236,47 @@ int mm_init(void)
     PUT(heap_listp+(3*WSIZE), PACK(0, 1));      //epilogue header, 
                                                 //the only bock with size 0
     heap_listp += (2*WSIZE);
+    heap_lastp = heap_listp;
 
     // Extend the empty heap with free block of CHUNKSIZE bytes
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
         return -1;
-    
+    nblocks += 2;
+    heap_walker();
     return 0;
 }
 
+void heap_walker(void)
+{
+    if(!VERBOSE)
+        return;
+    int block_cnt = 0;
+    char *bp = heap_listp;
+    while (1)
+    {
+        size_t size = GET_SIZE(HDRP(bp));
+        if (size == 0)
+        {
+            printf("Epilogure block encoutered @ %p\n", HDRP(bp));
+            block_cnt++;
+            break;
+        }
+        
+        if (GET_ALLOC(HDRP(bp)) && 0x1)
+        {
+            printf("BUSY BLOCK %d START %p, END %p, SIZE %d\n", block_cnt, HDRP(bp), FTRP(bp), GET_SIZE(HDRP(bp)));
+        }
+        else
+        {
+            printf("free block %d start %p, end %p, size %d\n", block_cnt, HDRP(bp), FTRP(bp), GET_SIZE(HDRP(bp)));
+        }
+        bp = NEXT_BLKP(bp);
+        block_cnt++;
+    }
+    printf("Totally %d blocks allocated.  Press [Enter] to continue . . .\n", block_cnt);
+    fflush(stdout);
+    getchar();
+}
 
 
 /* 
@@ -196,7 +287,11 @@ int mm_init(void)
 void *mm_malloc(size_t size)
 {
     size_t asize;
-    size_t extendsize;
+    
+    if (VERBOSE)
+    {
+        printf("Request raw size %d \n", size);
+    }
 
     char *bp;
 
@@ -212,17 +307,27 @@ void *mm_malloc(size_t size)
         asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
     
     // Search free list for a fit
-    if ((bp = find_fit(asize)) != NULL)
+    if ((bp = find_first_fit(asize)) != NULL)
     {
         place(bp, asize);
+        if (VERBOSE)
+        {
+            printf("Alloc at %p raw size %d \n", HDRP(bp), size);
+        }
+        heap_walker();
         return bp;
     }
 
     // No fit found, Get more memory and place the block
-    extendsize = MAX(asize, CHUNKSIZE);
+    size_t extendsize = MAX(asize, CHUNKSIZE);
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
         return NULL;
     place(bp, asize);
+    if (VERBOSE)
+    {
+        printf("Alloc at %p raw size %d after extending heap.\n", HDRP(bp), size);
+    }
+    heap_walker();
     return bp;
 }
 
@@ -232,10 +337,15 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    if (VERBOSE)
+    {
+        printf("Free memory %p\n", HDRP(ptr));
+    }
     size_t size = GET_SIZE(HDRP(ptr));
     PUT(HDRP(ptr), PACK(size, 0));
     PUT(FTRP(ptr), PACK(size, 0));
     coalesce(ptr);
+    heap_walker();
 }
 
 /*
